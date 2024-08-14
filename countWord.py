a16b705a55c71a0ee4a91e8f34d0a324b4735b2e
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-08-14 23:27:01 +0800

    Update and rename CC.py to countWord.py

---
+++ countWord.py
@@ -0,0 +1,43 @@
+song = """Old MacDonald had a farm,
+E I E I O
+And on that farm he had a cow,
+E I E I O
+With a moo moo here and a moo moo there,
+Here a moo, there a moo, everywhere a moo moo.
+Old MacDonald had a farm,
+E I E I O
+Old MacDonald had a farm,
+E I E I O
+And on that farm he had a duck,
+E I E I O
+With a quack quack here and a quack quack there,
+Here a quack, there a quack, everywhere a quack quack.
+Old MacDonald had a farm,
+E I E I O
+Old MacDonald had a farm,
+E I E I O
+And on that farm he had a horse,
+E I E I O
+With a neigh neigh here and a neigh neigh there,
+Here a neigh, there a neigh, everywhere a neigh neigh.
+Old MacDonald had a farm,
+E I E I O
+Old MacDonald had a farm,
+E I E I O
+And on that farm he had a pig,
+E I E I O
+With an oink oink here and an oink oink there,
+Here an oink, there an oink, everywhere an oink oink.
+Old MacDonald had a farm,
+E I E I O
+Old MacDonald had a farm,
+E I E I O"""
+
+lower_word = #how to do lower case
+replace_ch = #how to replace every ","
+song_list = #how to turn string in to a list
+for word in song_list:
+    #how to count each word in using a dict
+
+
+print(f"Word count: {mydict}")
